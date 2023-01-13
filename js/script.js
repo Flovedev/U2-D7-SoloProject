413e7a5a374e7cd99344a1a3e9f57c3ea6a2fb86
Flovedev <flove.dev@gmail.com>
2023-01-13 15:59:16 +0000

    update before 17

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-// function addMedia() {
-//     let trendingNode = document.getElementById('trending-container')
-//     let againNode = document.getElementById('again-container')
-//     let newNode = document.getElementById('new-container')
-
-//     for (let index = 0; index < 6; index++) {
-//         trendingNode.innerHTML += `<div class="col-12 col-sm-6 col-md-3 col-lg-2 col-xl-2 px-1">
-//         <img class="img-fluid" src="./assets/media/media${index}.jpg" alt="whatever">
-//         </div>`;
-//         againNode.innerHTML += `<div class="col-12 col-sm-6 col-md-3 col-lg-2 col-xl-2 px-1">
-//         <img class="img-fluid" src="./assets/media/media${index + 6}.jpg" alt="whatever">
-//         </div>`;
-//         newNode.innerHTML += `<div class="col-12 col-sm-6 col-md-3 col-lg-2 col-xl-2 px-1">
-//         <img class="img-fluid" src="./assets/media/media${index + 12}.jpg" alt="whatever">
-//         </div>`;
-//     }
-// }
 function addMedia() {
     let trendingDisplayNode = document.getElementById('trending-display')
     let trendingOtherNode = document.getElementById('trending-others')
@@ -24,15 +7,13 @@ function addMedia() {
     let newOtherNode = document.getElementById('new-others')
 
     for (let index = 0; index < 6; index++) {
-        trendingDisplayNode.innerHTML += `<div class="col p-1"><img src="./assets/media/media${index}.jpg" class="w-100" alt="..."></div>`;
-        trendingOtherNode.innerHTML += `<div class="col p-1"><img src="./assets/media/media${index + 6}.jpg" class="w-100" alt="..."></div>`;
-        againDisplayNode.innerHTML += `<div class="col p-1"><img src="./assets/media/media${index + 12}.jpg" class="w-100" alt="..."></div>`;
-        againOtherNode.innerHTML += `<div class="col p-1"><img src="./assets/media/media${index + 18}.jpg" class="w-100" alt="..."></div>`;
-        newDisplayNode.innerHTML += `<div class="col p-1"><img src="./assets/media/media${index + 24}.jpg" class="w-100" alt="..."></div>`;
-        newOtherNode.innerHTML += `<div class="col p-1"><img src="./assets/media/media${index + 27}.jpg" class="w-100" alt="..."></div>`;
-
+        trendingDisplayNode.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-2 p-1"><img src="./assets/media/media${index}.jpg" class="w-100" alt="..."></div>`;
+        trendingOtherNode.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-2 p-1"><img src="./assets/media/media${index + 6}.jpg" class="w-100" alt="..."></div>`;
+        againDisplayNode.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-2 p-1"><img src="./assets/media/media${index + 12}.jpg" class="w-100" alt="..."></div>`;
+        againOtherNode.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-2 p-1"><img src="./assets/media/media${index + 18}.jpg" class="w-100" alt="..."></div>`;
+        newDisplayNode.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-2 p-1"><img src="./assets/media/media${index + 24}.jpg" class="w-100" alt="..."></div>`;
+        newOtherNode.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-2 p-1"><img src="./assets/media/media${index + 27}.jpg" class="w-100" alt="..."></div>`;
     }
 }
-
 
 window.onload(addMedia())
